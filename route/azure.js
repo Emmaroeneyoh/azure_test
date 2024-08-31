5f80a1d7666c08f89988feb41ddd48d5b3602267
emmaro <your-new-email@example.com>
2024-08-31 07:01:16 +0100

    first commit

--- route/azure.js
+++ route/azure.js
@@ -2,6 +2,8 @@ const { usercreateaddressController, usertestapiController } = require("../app/c
 
 const router = require("express").Router();
 
+
+router.get('/api/notifications', usercreateaddressController);
 router.post("/api/notifications", usercreateaddressController);
 router.get("/test", usertestapiController);
 
